f93bdbe4abd591d3c13bff463a2e5180a52f0253
Zhanghualei <zhanghualei@bonc.com.cn>
2021-01-21 16:19:34 +0800

    自定义元数据使用

--- microservice-simple-consumer-movie/src/main/java/com/zhl/microservicesimpleconsumermovie/user/controller/MovieController.java
+++ microservice-simple-consumer-movie/src/main/java/com/zhl/microservicesimpleconsumermovie/user/controller/MovieController.java
@@ -2,20 +2,26 @@ package com.zhl.microservicesimpleconsumermovie.user.controller;
 
 import com.zhl.microservicesimpleconsumermovie.user.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 /**
- * @author zhouli
+ * @author zhl
  */
 @RequestMapping("/movies")
 @RestController
 public class MovieController {
   @Autowired
   private RestTemplate restTemplate;
+  @Autowired
+  private DiscoveryClient discoveryClient;
 
   @GetMapping("/users/{id}")
   public User findById(@PathVariable Long id) {
@@ -23,5 +29,10 @@ public class MovieController {
     User user = this.restTemplate.getForObject("http://localhost:8000/{id}", User.class, id);
     // ...电影微服务的业务...
     return user;
+  }
+
+  @GetMapping("/user-instance")
+  public List<ServiceInstance> showInfo(){
+    return this.discoveryClient.getInstances("microservice-provider-user");
   }
 }
